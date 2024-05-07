e4351f66dc5ede4b225e3ef34bd642d1762d9501
Sarai Thomas <starr@sarais-air.mynetworksettings.com>
2024-05-07 01:09:49 -0400

    Removed unnecessary dummy data added by creating react app

---
+++ Body-Flow-Dashboard/frontend/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="">
+        <a
+          className=""
+          href="https://github.com/sarai-ii/BodyFlow-Front-End"
+        >
+          Local Repository
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
