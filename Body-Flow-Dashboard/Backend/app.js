90b8fa0195c2d37ab1e2a3c401f3522d924f45d9
Sarai Thomas <starr@sarais-air.mynetworksettings.com>
2024-05-17 14:18:19 -0400

    Created basic express app with health check route

---
+++ Body-Flow-Dashboard/Backend/app.js
@@ -0,0 +1,15 @@
+// DEPENDENCIES
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+
+//MIDDLEWARE
+require("dotenv").config
+app.use(cors());
+
+//CONTROLLERS
+//HEALTH CHECK ROUTE
+app.get("/", (req, res) => res.status(200).json({data: `Hello World`}))
+
+module.exports = app
