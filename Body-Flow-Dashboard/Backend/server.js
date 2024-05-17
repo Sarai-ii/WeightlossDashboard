70e4c0b8f48f080b71e4e9a12e8956a5adcc40a4
Sarai Thomas <starr@sarais-air.mynetworksettings.com>
2024-05-17 14:31:48 -0400

    Health check is working, safe to move on to building app

--- Body-Flow-Dashboard/Backend/server.js
+++ Body-Flow-Dashboard/Backend/server.js
@@ -1,3 +1,6 @@
+require('dotenv').config()
 const app = require('./app');
 const PORT = process.env.PORT || 9000
-app.listen(PORT, console.log(`Listening on port ${PORT}....`))
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}....`)
+})
